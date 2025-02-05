85ead45cb2274aa2d6de564635cfa3d235d0511d
Hamza Amin <mh4070685@gmail.com>
2025-02-05 22:24:07 +0500

    minor changes 6

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 const client = "6b5157bf3245407e8ad12b2c23b1dd82";
 const secret = "70822fc7b0e846f5badc5b25c1c0539d";
-const refresh = "AQBMV5JWks9b5qSORs-G6vBb0y56nKyUgKeaOTvJXz6Yxmn0Y8VDtNMzmqHJJ5nydTtW-pkTYg9QbcqD7sAobfVS9C6LDuxyotfNbAjBL1jN_eEmF0iEzOYIfuM-6cTYDpOdyGxB944cfyyXoY1i42MNX82MzzYljq8RqyDQdade9vqi1h2ZtPMp8rD1DUSmrbsCazHNBI8b5oyC7m3aLNQ1ln4X0fl7hGVtnjROoUaWTNdGNNsjALr-9g"
+const refresh = "AQCWBUXRrlUUUic4mu8C-EjtGkorhP2jPn7J03RlfPx-YT4Pif9l9muubeotdc3-kdRo5mLLMkYY6LeZdchSfTc6HYf_VIHD3kUEcixBF6eREZdjRd1bKEhVlxAPMJpyZDA"
 
 const basic = btoa(`${client}:${secret}`);
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
